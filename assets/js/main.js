6c0f45d5d62624a30b039248294be39333d94950
unknown <ebranafas@gmail.com>
2022-03-06 10:00:25 +0330

    checker persian and arabic number

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,21 @@ const txtCredits = document.querySelectorAll('.box__input input');
 const detectedbank_box = document.querySelector('.box-detectedbank');
 let firstNumCredit = undefined;
 let checkNumber = false;
+const persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g];
+const arabicNumbers  = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g];
 
+
+const ReplaceDigitNumbers = (value) => {
+    if(typeof value === "string"){
+        for(let i = 0; i < 10; i++){
+            value = value.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
+        }
+    }
+    return value;
+}
 
 const DetectInput = (event, index) => {
+    event.target.value = ReplaceDigitNumbers(event.target.value);
     if(checkNumber){
         if(index === 3){
             return false;
@@ -53,9 +65,11 @@ const DetectCreditCard = (value) => {
 }
 
 
+window.addEventListener('load', ReplaceDigitNumbers);
+
 if(txtCredits){
     txtCredits.forEach((txtCredit, index) => {
         txtCredit.addEventListener('keypress', (e) => NumberValidation(e));
         txtCredit.addEventListener('input', (e) => DetectInput(e, index));
-    })
+    });
 }
